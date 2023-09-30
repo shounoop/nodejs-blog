8668fc5527b09662678508c374bea41e3da4708f
Shounoop <hmtuan9a@gmail.com>
2023-09-30 15:34:45 +0700

    refactor: created routes, app/controllers folder (mvc model)

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
+const route = require('./routes');
 
+// Init app
 const app = express();
 const port = 3000;
 
+// Static file
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -15,32 +19,14 @@ app.use(express.json());
 app.use(morgan('combined'));
 
 // Template engine
-app.engine(
-	'hbs',
-	engine({
-		extname: '.hbs',
-	})
-);
+app.engine('hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
-app.get('/', (req, res) => {
-	res.render('home');
-});
+// Routes init
+route(app);
 
-app.get('/news', (req, res) => {
-	res.render('news');
-});
-
-app.get('/search', (req, res) => {
-	res.render('search');
-});
-
-app.post('/search', (req, res) => {
-	console.log(req.body);
-	res.send('');
-});
-
+// Listen on port
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });

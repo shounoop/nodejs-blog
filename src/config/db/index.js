5043cc8f207fd5c8454b30c2b031dbde96546e02
Shounoop <hmtuan9a@gmail.com>
2023-10-02 14:50:15 +0700

    [crud] delete course

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
 	try {
-		await mongoose.connect('mongodb://127.0.0.1/f8_education_dev');
+		await mongoose.connect('mongodb://127.0.0.1/f8_education_dev', {});
 
 		console.log('connect to DB successfully!!!');
 	} catch (error) {
